0558021baf7875919c1890dd2650a5d188b28dc3
zhutou975369 <guanhr8711@yahoo.cb>
2012-07-15 16:51:29 -0400

    initial commit

---
+++ SchooldateMobile/SchooldateMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SchooldateMobile
+//
+//  Created by Haoran Guan on 7/15/12.
+//  Copyright (c) 2012 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SchooldateMobile/SchooldateMobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SchooldateMobile
+//
+//  Created by Haoran Guan on 7/15/12.
+//  Copyright (c) 2012 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
